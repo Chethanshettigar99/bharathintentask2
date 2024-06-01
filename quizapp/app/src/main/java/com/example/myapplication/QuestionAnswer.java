1ebf2baeb0898e22497383809ed5d6e0c6c30762
chethanshettigar99 <chethanshettigar099@gmail.com>
2024-06-01 21:37:23 +0530

    first commit

---
+++ quizapp/app/src/main/java/com/example/myapplication/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+public class QuestionAnswer {
+
+    public static String question []={
+            "Which is largest hot desert ?",
+            "Who is father of computer ",
+            "what is 50/2 ?",
+            "who is prime minister of india",
+            "What is national Anthem of India"
+    };
+    public static String choices [][]={
+            {"thar" , "namib" , "sahara" , "guban"},
+            {"charles babbage" , "alan turing" , "newton" , "edison"},
+            {"30" , "20" , "10" , "25"},
+            {"Rahul gandhi" , "Narendra modi" , "Amit shah" , "Rajnath singh"},
+            {"jhanda uncha" , "saare jahan se acha" , "vandhe matharam" , "jana gana mana"}
+    };
+    public static String correctAnswers []={
+            "sahara" ,
+            "charles babbage",
+            "25",
+            "Narendra modi",
+            "jana gana mana"};
+}
